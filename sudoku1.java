6994c8948eb372b42c338cb4141212d85d03acda
ia5699 <54573155+ia5699@users.noreply.github.com>
2021-05-27 22:04:18 +0530

    Add files via upload

---
+++ sudoku1.java
@@ -0,0 +1,91 @@
+package sudoku;
+import java.util.*;
+public class sudoku1 {
+
+    public static void main(String[] args) {
+        int  matrix[][] = new int[9][9];
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the sudoku quiz");
+        for(int i=0;i<9;i++)
+        {
+        	for(int j=0;j<9;j++)
+        	{
+        		matrix[i][j]=sc.nextInt();
+        	}
+        }
+        if(SolveSuduko(matrix, 9)){
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 9; j++) {
+                    System.out.print(matrix[i][j]+" ");
+                }
+                System.out.println();
+            }
+        }
+
+    }
+
+    public static boolean SolveSuduko(int [][]matrix, int n){
+        int rowIndex = -1;
+        int columnIndex = -1;
+        int i = 0;
+        int j = 0;
+
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < n; j++) {
+                if(matrix[i][j] == 0){
+                    rowIndex = i;
+                    columnIndex = j;
+                    break;
+                }
+            }
+            if(rowIndex != -1){
+                break;
+            }
+
+        }
+        if(i == n && j == n){
+            return  true;
+        }
+        else {
+            for (int value = 1; value < 10; value++) {
+                if(IsSafe(matrix, value, rowIndex, columnIndex, n)){
+                    matrix[rowIndex][columnIndex] = value;
+                    if(!SolveSuduko(matrix,n)){
+                        matrix[rowIndex][columnIndex] = 0;
+                    }
+                    else {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+
+    public static boolean IsSafe(int [][] matrix, int value, int rowIndex, int columnIndex, int n){
+        //row check
+        for (int j = 0; j < 9; j++) {
+            if(matrix[rowIndex][j] == value){
+                return  false;
+            }
+        }
+        //column check
+        for (int i = 0; i < 9; i++) {
+            if(matrix[i][columnIndex] == value){
+                return  false;
+            }
+        }
+        //submatrix check
+        int baseRowIndex = rowIndex - (rowIndex % 3);
+        int baseColumnIndex = columnIndex - (columnIndex % 3);
+        for (int i = baseRowIndex; i < baseRowIndex + 3; i++) {
+            for (int j = baseColumnIndex; j < baseColumnIndex + 3; j++) {
+                if(matrix[i][j] == value){
+                    return  false;
+                }
+
+            }
+        }
+        return true;
+    }
+}
